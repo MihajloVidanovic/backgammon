ceb037d746cc8673f53e5e17b85948a6dc1faa2f
Mihajlo Vidanović <149157206+MihajloVidanovic@users.noreply.github.com>
2024-04-19 17:54:49 +0200

    Added base for pieces drawing system

--- main.cpp
+++ main.cpp
@@ -55,6 +55,56 @@ class Node {
 
 };
 
+void draw_pieces(Texture2D sheet) {
+    for(int i = 1; i <= 24; i++) {
+        if(board[i-1] != 0) {
+            switch(i-1) {
+                case 1:
+                case 2:
+                case 3:
+                case 4:
+                case 5:
+                case 6:
+                    // 288 486 y mora biti oduzet
+                    
+                    break;
+                case 7:
+                case 8:
+                case 9:
+                case 10:
+                case 11:
+                case 12:
+                    // 32 486 y mora biti oduzet
+                    
+                    break;
+                case 13:
+                case 14:
+                case 15:
+                case 16:
+                case 17:
+                case 18:
+                    // 32, 116
+                    for(int j = 0; j < board[i-1]; j++) {
+                        if(board[i-1] > 0) {
+                            DrawTexturePro(sheet, (Rectangle){32, 104, 8, 8}, (Rectangle){(float)(32 + (i - 13) * 32), 116 + j * 32, 32, 32}, (Vector2){0.0f, 0.0f}, 0.0f, WHITE); }
+                        else {
+                            DrawTexturePro(sheet, (Rectangle){40, 104, 8, 8}, (Rectangle){(float)(32 + (i - 13) * 32), 116 + j * 32, 32, 32}, (Vector2){0.0f, 0.0f}, 0.0f, WHITE); }
+                    }
+                    break;
+                case 19:
+                case 20:
+                case 21:
+                case 22:
+                case 23:
+                case 24:
+                    // 288, 116
+                    
+                    break;
+            }
+        }
+    }
+}
+
 int main()
 {
     InitWindow(512, 512, "Backgammon");
@@ -69,7 +119,11 @@ int main()
         
             ClearBackground(RAYWHITE);
             
+            // background
             DrawTexturePro(spritesheet, (Rectangle){48, 0, 128, 128}, (Rectangle){0, 0, 512, 512}, (Vector2){0.0f, 0.0f}, 0.0f, WHITE);
+            
+            // draw pieces
+            draw_pieces(spritesheet);
         
         EndDrawing();
     }
