fbc2b0c4ee54483f1f078960573cbd3126c2cf6e
MihajloVidanovic <vidanovicmihajlo07@gmail.com>
2024-04-24 13:14:13 +0200

    Pieces drawing done

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@
 
 // Starting position
 char board[24] = {2, 0, 0, 0, 0, -5, 0, -3, 0, 0, 0, 5, 
-                            -5, 0, 0, 0, 3, 0, 5, 0, 0, 0, 0, -2};
+                 -5, 0, 0, 0, 3, 0, 5, 0, 0, 0, 0, -2};
 
 // starts at 32 116
 
@@ -55,55 +55,7 @@ class Node {
 
 };
 
-void draw_pieces(Texture2D sheet) {
-    for(int i = 1; i <= 24; i++) {
-        if(board[i-1] != 0) {
-            switch(i-1) {
-                case 1:
-                case 2:
-                case 3:
-                case 4:
-                case 5:
-                case 6:
-                    // 288 486 y mora biti oduzet
-                    
-                    break;
-                case 7:
-                case 8:
-                case 9:
-                case 10:
-                case 11:
-                case 12:
-                    // 32 486 y mora biti oduzet
-                    
-                    break;
-                case 13:
-                case 14:
-                case 15:
-                case 16:
-                case 17:
-                case 18:
-                    // 32, 116
-                    for(int j = 0; j < board[i-1]; j++) {
-                        if(board[i-1] > 0) {
-                            DrawTexturePro(sheet, (Rectangle){32, 104, 8, 8}, (Rectangle){(float)(32 + (i - 13) * 32), 116 + j * 32, 32, 32}, (Vector2){0.0f, 0.0f}, 0.0f, WHITE); }
-                        else {
-                            DrawTexturePro(sheet, (Rectangle){40, 104, 8, 8}, (Rectangle){(float)(32 + (i - 13) * 32), 116 + j * 32, 32, 32}, (Vector2){0.0f, 0.0f}, 0.0f, WHITE); }
-                    }
-                    break;
-                case 19:
-                case 20:
-                case 21:
-                case 22:
-                case 23:
-                case 24:
-                    // 288, 116
-                    
-                    break;
-            }
-        }
-    }
-}
+void draw_pieces(Texture2D sheet);
 
 int main()
 {
@@ -130,4 +82,69 @@ int main()
     
     CloseWindow();
     return 0;
+}
+
+void draw_pieces(Texture2D sheet) {
+    for(int i = 1; i <= 24; i++) {
+        if(board[i-1] != 0) {
+            switch(i) {
+                case 1:
+                case 2:
+                case 3:
+                case 4:
+                case 5:
+                case 6:
+                    // 288 486 y mora biti oduzet
+                    for(int j = 0; j < abs(board[i-1]); j++) {
+                        if((int)board[i-1] > 0) {
+                            DrawTexturePro(sheet, (Rectangle){40, 104, 8, 8}, (Rectangle){(float)(288 + abs(i - 6) * 32), (float)(436 - j * 32), 32, 32}, (Vector2){0.0f, 0.0f}, 0.0f, WHITE); }
+                        else {
+                            DrawTexturePro(sheet, (Rectangle){32, 104, 8, 8}, (Rectangle){(float)(288 + abs(i - 6) * 32), (float)(436 - j * 32), 32, 32}, (Vector2){0.0f, 0.0f}, 0.0f, WHITE); }
+                    }
+                    break;
+                case 7:
+                case 8:
+                case 9:
+                case 10:
+                case 11:
+                case 12:
+                    // 32 486 y mora biti oduzet
+                    for(int j = 0; j < abs(board[i-1]); j++) {
+                        if((int)board[i-1] > 0) {
+                            DrawTexturePro(sheet, (Rectangle){40, 104, 8, 8}, (Rectangle){(float)(32 + abs(i - 12) * 32), (float)(436 - j * 32), 32, 32}, (Vector2){0.0f, 0.0f}, 0.0f, WHITE); }
+                        else {
+                            DrawTexturePro(sheet, (Rectangle){32, 104, 8, 8}, (Rectangle){(float)(32 + abs(i - 12) * 32), (float)(436 - j * 32), 32, 32}, (Vector2){0.0f, 0.0f}, 0.0f, WHITE); }
+                    }
+                    break;
+                case 13:
+                case 14:
+                case 15:
+                case 16:
+                case 17:
+                case 18:
+                    // 32, 116
+                    for(int j = 0; j < abs(board[i-1]); j++) {
+                        if((int)board[i-1] > 0) {
+                            DrawTexturePro(sheet, (Rectangle){40, 104, 8, 8}, (Rectangle){(float)(32 + (i - 13) * 32), (float)(116 + j * 32), 32, 32}, (Vector2){0.0f, 0.0f}, 0.0f, WHITE); }
+                        else {
+                            DrawTexturePro(sheet, (Rectangle){32, 104, 8, 8}, (Rectangle){(float)(32 + (i - 13) * 32), (float)(116 + j * 32), 32, 32}, (Vector2){0.0f, 0.0f}, 0.0f, WHITE); }
+                    }
+                    break;
+                case 19:
+                case 20:
+                case 21:
+                case 22:
+                case 23:
+                case 24:
+                    // 288, 116
+                    for(int j = 0; j < abs(board[i-1]); j++) {
+                        if((int)board[i-1] > 0) {
+                            DrawTexturePro(sheet, (Rectangle){40, 104, 8, 8}, (Rectangle){(float)(288 + (i - 19) * 32), (float)(116 + j * 32), 32, 32}, (Vector2){0.0f, 0.0f}, 0.0f, WHITE); }
+                        else {
+                            DrawTexturePro(sheet, (Rectangle){32, 104, 8, 8}, (Rectangle){(float)(288 + (i - 19) * 32), (float)(116 + j * 32), 32, 32}, (Vector2){0.0f, 0.0f}, 0.0f, WHITE); }
+                    }
+                    break;
+            }
+        }
+    }
 }
